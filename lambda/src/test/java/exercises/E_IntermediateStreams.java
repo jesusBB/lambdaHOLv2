a0de47e9b66247cfdc0532d4ab8c83e6fad49f43
Jesus Bustillo <jesus.bustillo@cdl.co.uk>
2020-12-04 11:50:05 +0000

    Changing E_IntermediateStreams

--- lambda/src/test/java/exercises/E_IntermediateStreams.java
+++ lambda/src/test/java/exercises/E_IntermediateStreams.java
@@ -13,6 +13,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
@@ -27,11 +29,12 @@ public class E_IntermediateStreams {
     /**
      * Convert a list of strings into a list of characters.
      */
-    @Test @Ignore
+    @Test
+    // @Ignore
     public void e1_stringsToCharacters() {
-        List<String> input = List.of("alfa", "bravo", "charlie");
+        final List<String> input = List.of("alfa", "bravo", "charlie");
 
-        List<Character> result = null; // TODO
+        final List<Character> result = input.stream().flatMap(s -> s.chars().mapToObj(i -> (char) i)).collect(Collectors.toList()); // TODO
 
         assertEquals("[a, l, f, a, b, r, a, v, o, c, h, a, r, l, i, e]", result.toString());
         assertTrue(result.stream().allMatch(x -> x instanceof Character));
@@ -45,7 +48,6 @@ public class E_IntermediateStreams {
     // Pay attention to the return type of String.chars() and boxing conversion.
     // </editor-fold>
 
-
     /**
      * Collect all the words from the text file into a list.
      * Use the regular expression Pattern SPLIT_PATTERN to split
@@ -57,33 +59,33 @@ public class E_IntermediateStreams {
      *
      * @throws IOException
      */
-    @Test @Ignore
+    @Test
+    // @Ignore
     public void e2_listOfAllWords() throws IOException {
-        List<String> output = null; // TODO
+        final List<String> output = reader.lines().flatMap(line -> SPLIT_PATTERN.splitAsStream(line)).collect(Collectors.toList()); // TODO
 
         assertEquals(
-            List.of(
-                "From", "fairest", "creatures", "we", "desire", "increase",
-                "That", "thereby", "beauty's", "rose", "might", "never", "die",
-                "But", "as", "the", "riper", "should", "by", "time", "decease",
-                "His", "tender", "heir", "might", "bear", "his", "memory",
-                "But", "thou", "contracted", "to", "thine", "own", "bright", "eyes",
-                "Feed'st", "thy", "light's", "flame", "with", "self", "substantial", "fuel",
-                "Making", "a", "famine", "where", "abundance", "lies",
-                "Thy", "self", "thy", "foe", "to", "thy", "sweet", "self", "too", "cruel",
-                "Thou", "that", "art", "now", "the", "world's", "fresh", "ornament",
-                "And", "only", "herald", "to", "the", "gaudy", "spring",
-                "Within", "thine", "own", "bud", "buriest", "thy", "content",
-                "And", "tender", "churl", "mak'st", "waste", "in", "niggarding",
-                "Pity", "the", "world", "or", "else", "this", "glutton", "be",
-                "To", "eat", "the", "world's", "due", "by", "the", "grave", "and", "thee"),
-            output);
+                List.of(
+                        "From", "fairest", "creatures", "we", "desire", "increase",
+                        "That", "thereby", "beauty's", "rose", "might", "never", "die",
+                        "But", "as", "the", "riper", "should", "by", "time", "decease",
+                        "His", "tender", "heir", "might", "bear", "his", "memory",
+                        "But", "thou", "contracted", "to", "thine", "own", "bright", "eyes",
+                        "Feed'st", "thy", "light's", "flame", "with", "self", "substantial", "fuel",
+                        "Making", "a", "famine", "where", "abundance", "lies",
+                        "Thy", "self", "thy", "foe", "to", "thy", "sweet", "self", "too", "cruel",
+                        "Thou", "that", "art", "now", "the", "world's", "fresh", "ornament",
+                        "And", "only", "herald", "to", "the", "gaudy", "spring",
+                        "Within", "thine", "own", "bud", "buriest", "thy", "content",
+                        "And", "tender", "churl", "mak'st", "waste", "in", "niggarding",
+                        "Pity", "the", "world", "or", "else", "this", "glutton", "be",
+                        "To", "eat", "the", "world's", "due", "by", "the", "grave", "and", "thee"),
+                output);
     }
     // Hint:
     // <editor-fold defaultstate="collapsed">
     // Use Stream.flatMap().
     // </editor-fold>
-
 
     /**
      * Read the words from the text file, and create a list containing the words
@@ -91,21 +93,23 @@ public class E_IntermediateStreams {
      *
      * @throws IOException
      */
-    @Test @Ignore
+    @Test
+    // @Ignore
     public void e3_longLowerCaseSortedWords() throws IOException {
-        List<String> output = null; // TODO
+        final List<String> output = reader.lines().flatMap(line -> SPLIT_PATTERN.splitAsStream(line)).filter(word -> word.length() >= 8)
+                .map(String::toLowerCase).sorted()
+                .collect(Collectors.toList());
 
         assertEquals(
-            List.of(
-                "abundance", "beauty's", "contracted", "creatures",
-                "increase", "niggarding", "ornament", "substantial"),
-            output);
+                List.of(
+                        "abundance", "beauty's", "contracted", "creatures",
+                        "increase", "niggarding", "ornament", "substantial"),
+                output);
     }
     // Hint:
     // <editor-fold defaultstate="collapsed">
     // Use Stream.sorted().
     // </editor-fold>
-
 
     /**
      * Read the words from the text file, and create a list containing the words
@@ -114,21 +118,22 @@ public class E_IntermediateStreams {
      *
      * @throws IOException
      */
-    @Test @Ignore
+    @Test
+    // @Ignore
     public void e4_longLowerCaseReverseSortedWords() throws IOException {
-        List<String> result = null; // TODO
+        final List<String> result = reader.lines().flatMap(line -> SPLIT_PATTERN.splitAsStream(line)).filter(word -> word.length() >= 8)
+                .map(String::toLowerCase).sorted(Comparator.reverseOrder()).collect(Collectors.toList()); // TODO
 
         assertEquals(
-            List.of(
-                "substantial", "ornament", "niggarding", "increase",
-                "creatures", "contracted", "beauty's", "abundance"),
-            result);
+                List.of(
+                        "substantial", "ornament", "niggarding", "increase",
+                        "creatures", "contracted", "beauty's", "abundance"),
+                result);
     }
     // Hint:
     // <editor-fold defaultstate="collapsed">
     // Use Comparator.reverseOrder().
     // </editor-fold>
-
 
     /**
      * Read words from the text file, and sort unique, lower-cased words by length,
@@ -136,26 +141,29 @@ public class E_IntermediateStreams {
      *
      * @throws IOException
      */
-    @Test @Ignore
+    @Test
+    // @Ignore
     public void e5_sortedLowerCaseDistinctByLengthThenAlphabetically() throws IOException {
-        List<String> result = null; // TODO
-
+        final List<String> result = reader.lines().flatMap(line -> SPLIT_PATTERN.splitAsStream(line)).map(String::toLowerCase).distinct()
+                .sorted(Comparator.comparing(String::length).thenComparing(Comparator.naturalOrder()))
+                .collect(Collectors.toList()); // TODO
+        System.out.println(result);
         assertEquals(
-            List.of(
-                "a", "as", "be", "by", "in", "or", "to", "we",
-                "and", "art", "bud", "but", "die", "due", "eat", "foe",
-                "his", "now", "own", "the", "thy", "too", "bear", "else",
-                "eyes", "from", "fuel", "heir", "lies", "only",
-                "pity", "rose", "self", "that", "thee", "this", "thou",
-                "time", "with", "churl", "cruel", "flame", "fresh", "gaudy",
-                "grave", "might", "never", "riper", "sweet", "thine",
-                "waste", "where", "world", "bright", "desire", "famine",
-                "herald", "mak'st", "making", "memory", "should", "spring",
-                "tender", "within", "buriest", "content", "decease",
-                "fairest", "feed'st", "glutton", "light's", "thereby", "world's", "beauty's",
-                "increase", "ornament", "abundance", "creatures", "contracted", "niggarding",
-                "substantial"),
-            result);
+                List.of(
+                        "a", "as", "be", "by", "in", "or", "to", "we",
+                        "and", "art", "bud", "but", "die", "due", "eat", "foe",
+                        "his", "now", "own", "the", "thy", "too", "bear", "else",
+                        "eyes", "from", "fuel", "heir", "lies", "only",
+                        "pity", "rose", "self", "that", "thee", "this", "thou",
+                        "time", "with", "churl", "cruel", "flame", "fresh", "gaudy",
+                        "grave", "might", "never", "riper", "sweet", "thine",
+                        "waste", "where", "world", "bright", "desire", "famine",
+                        "herald", "mak'st", "making", "memory", "should", "spring",
+                        "tender", "within", "buriest", "content", "decease",
+                        "fairest", "feed'st", "glutton", "light's", "thereby", "world's", "beauty's",
+                        "increase", "ornament", "abundance", "creatures", "contracted", "niggarding",
+                        "substantial"),
+                result);
     }
     // Hint 1:
     // <editor-fold defaultstate="collapsed">
@@ -170,9 +178,14 @@ public class E_IntermediateStreams {
      * Compute the value of 21!, that is, 21 factorial. This value is larger than
      * Long.MAX_VALUE, so you must use BigInteger.
      */
-    @Test @Ignore
+    @Test
+    // @Ignore
     public void e6_bigFactorial() {
-        BigInteger result = BigInteger.ONE; // TODO
+        final BigInteger result = LongStream.rangeClosed(1, 21)
+                .mapToObj(BigInteger::valueOf)
+                .reduce(BigInteger::multiply).orElse(BigInteger.ONE);
+        // Alternative
+        // .reduce(BigInteger.ONE, BigInteger::multiply);
 
         assertEquals(new BigInteger("51090942171709440000"), result);
     }
@@ -187,15 +200,15 @@ public class E_IntermediateStreams {
     // a single value.
     // </editor-fold>
 
-
     /**
      * Get the last word in the text file.
      *
      * @throws IOException
      */
-    @Test @Ignore
+    @Test
+    // @Ignore
     public void e7_getLastWord() throws IOException {
-        String result = null; // TODO
+        final String result = reader.lines().flatMap(line -> SPLIT_PATTERN.splitAsStream(line)).reduce((a, b) -> b).orElse(""); // TODO
 
         assertEquals("thee", result);
     }
@@ -207,15 +220,16 @@ public class E_IntermediateStreams {
     /**
      * Create a list containing ArrayList.class and all its super classes.
      */
-    @Test @Ignore
+    @Test
+    // @Ignore
     public void e8_selectTheSuperClassesOfArrayList() {
-        Class<?> origin = ArrayList.class;
+        final Class<?> origin = ArrayList.class;
 
-        List<String> result = null; // TODO
+        final List<Class<?>> result = Stream.<Class<?>> iterate(origin, Class::getSuperclass).takeWhile(Objects::nonNull).collect(Collectors.toList()); // TODO
 
         assertEquals(
-            List.of(ArrayList.class, AbstractList.class, AbstractCollection.class, Object.class),
-            result);
+                List.of(ArrayList.class, AbstractList.class, AbstractCollection.class, Object.class),
+                result);
     }
     // Hint:
     // <editor-fold defaultstate="collapsed">
@@ -225,24 +239,24 @@ public class E_IntermediateStreams {
     // Java 9 added the takeWhile() method on the stream interface.
     // </editor-fold>
 
-
     /**
      * Count the length of a stream dropping the first elements on a predicate.
      */
-    @Test @Ignore
+    @Test
+    @Ignore
     public void e9_countTheElementsAfterAPredicate() {
 
-        Random rand = new Random(314L);
-        Stream<String> stream = Stream.iterate(
+        final Random rand = new Random(314L);
+        final Stream<String> stream = Stream.iterate(
                 "",
                 (String s) -> {
                     final int nextInt = rand.nextInt(10);
-                    return (nextInt == 0 && !s.isEmpty()) ? s.substring(0, s.length() - 1) :
-                           (nextInt == 8 || nextInt == 9) ? s + "+"
-                                                          : s;
+                    return (nextInt == 0 && !s.isEmpty()) ? s.substring(0, s.length() - 1)
+                            : (nextInt == 8 || nextInt == 9) ? s + "+"
+                                    : s;
                 }).limit(100);
 
-        long count = 0L; // TODO
+        final long count = 0L; // TODO
 
         assertEquals(53, count);
     }
@@ -251,12 +265,10 @@ public class E_IntermediateStreams {
     // Java 9 added the dropWhile() method on the stream interface.
     // </editor-fold>
 
-
-// ========================================================
-// END OF EXERCISES
-// TEST INFRASTRUCTURE IS BELOW
-// ========================================================
-
+    // ========================================================
+    // END OF EXERCISES
+    // TEST INFRASTRUCTURE IS BELOW
+    // ========================================================
 
     // Pattern for splitting a string into words
     static final Pattern SPLIT_PATTERN = Pattern.compile("[- .:,]+");
